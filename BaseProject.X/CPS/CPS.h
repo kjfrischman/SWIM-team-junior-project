08a4f0e593d8b4fb5d34aba461c5e3a531a9a85c
PeterStine <peter.stine@oit.edu>
2019-02-25 17:35:37 -0800

    Added CPS files, non-functional

---
+++ BaseProject.X/CPS/CPS.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/ This file contains the drivers for the Conductive Presence sensor.
+// 2/18/2019
+// Peter Stine
+// CPS.h
+// Junior Project
+
+#include <xc.h>
+#include <sys/attribs.h>
+#include "CPS.h"
+
+extern int water_detected;
+
+void CPS_config(void);
+
+
